97ebf90cff26dcd69b920ebee75060415e4022ee
fatimazahra-12 <fatimazahrabourajlate@gmail.com>
2026-01-01 16:14:35 +0100

    Add socket connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import http from "http";
+import { initSocket } from "./socket";
 
 const app: Express = express();
 const port = Number(process.env.SERVER_PORT) || 8085;
+const server = http.createServer(app);
+
+initSocket(server);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
